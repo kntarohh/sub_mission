1f2133dbbbd70a8504baaf4fb3d78befdbf935f9
kntarohh <tarokenboo93@gmail.com>
2019-09-30 16:06:08 +0000

    create User

---
+++ db/migrate/20190930155251_add_columns_to_users.rb
@@ -0,0 +1,12 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :self_introduction, :text
+    add_column :users, :website, :string
+  end
+end
