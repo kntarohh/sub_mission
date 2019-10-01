217764e8a65304db7e316e6a1911d2bc25053ee8
kntarohh <tarokenboo93@gmail.com>
2019-10-01 13:24:09 +0000

    create home-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks',
+                                  registrations: 'users/registrations',
+                                  # passwords: 'users/passwords'
+  }
+  root 'static_pages#home'
+  get  '/home',   to: 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
